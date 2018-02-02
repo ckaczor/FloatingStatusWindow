115600209daf1334dbad6eb1ddff66bdc6c74410
Chris Kaczor <chris@kaczorzoo.net>
2018-02-01 21:35:42 -0500

    Fix appearance window size

--- FloatingStatusWindowLibrary/Properties/Resources.Designer.cs
+++ FloatingStatusWindowLibrary/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace FloatingStatusWindowLibrary.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -210,6 +210,16 @@ namespace FloatingStatusWindowLibrary.Properties {
         public static string ForegroundColor {
             get {
                 return ResourceManager.GetString("ForegroundColor", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        public static System.Drawing.Icon gear {
+            get {
+                object obj = ResourceManager.GetObject("gear", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
         
